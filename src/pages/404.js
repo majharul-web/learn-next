0ccd60388b383b7a51bb3a66379b85a1e8feb42e
majharul <majharuljony163@gmail.com>
2023-06-04 18:14:19 +0600

    [Done] learn next routing

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+const PageNotFound = () => {
+  return (
+    <div>
+      <h3>Your requested page was not found!</h3>
+    </div>
+  );
+};
+
+export default PageNotFound;
--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -1,10 +1,17 @@
+import { useRouter } from "next/router";
+
 const Product = () => {
+  const router = useRouter();
+  const handlePlaceOrder = () => {
+    router.push("/");
+  };
   return (
     <div>
       <ul>
         <li>Product 1</li>
         <li>Product 2</li>
         <li>Product 3</li>
+        <button onClick={() => handlePlaceOrder()}>place order</button>
       </ul>
     </div>
   );

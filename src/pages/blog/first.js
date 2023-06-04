87541eab1a0acafb64d1f9ddc905f0065ec7a8c0
majharul <majharuljony163@gmail.com>
2023-06-04 17:20:29 +0600

    learn nested routing

---
+++ src/pages/blog/first.js
@@ -0,0 +1,9 @@
+const First = () => {
+  return (
+    <div>
+      <h3> Blog First</h3>
+    </div>
+  );
+};
+
+export default First;
---
+++ src/pages/blog/index.js
@@ -0,0 +1,9 @@
+const Blog = () => {
+  return (
+    <div>
+      <h3>Blog Home </h3>
+    </div>
+  );
+};
+
+export default Blog;
